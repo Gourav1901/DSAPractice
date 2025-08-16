c751eadd6bd40561be158a25d7a75fa8ded8598d
Gourav1901 <gouravchoudhary1002@gmail.com>
2025-08-16 14:22:31 +0530

    constructor

--- js/this.js
+++ js/this.js
@@ -18,3 +18,18 @@ function User(name, age) {
 }
 
 console.log(User("Gourav", 20));
+
+// Constructors in Javascript are a special kind of function used to create and instantiate objects,
+//  especially when multiple objects of the same kind need to be created.
+
+var a = new Object();
+var b = new String("raj");
+var c = new Number("24");
+var d = new Boolean(true);
+var e = new Array();
+var f = new Function();
+var g = new Date();
+var h = new RegExp();
+var i = new Error();
+
+console.log(a, b, c, d, e, f, g, h, i);
